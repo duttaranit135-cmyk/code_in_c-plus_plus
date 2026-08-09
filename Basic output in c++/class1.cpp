c5a0ed303623863fa2ecd92ac48c959706c3eea6
Ranit Dutta <duttaranit135@gmail.com>
2026-08-09 21:38:03 +0530

    Add files via upload

---
+++ Basic output in c++/class1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "hello world" << endl;
+    cout << "language in c++" << endl;
+    cout << "my name ranit" << endl;
+    cout << "my laptop dellg15" << endl;
+    cout << "coding and drawing is my hobby" << endl;
+    cout << "c++ was developed by bjarne stroustrup" << endl;
+    // this is frist code in c++
+
+    return 0;
+}
---
+++ Basic output in c++/class2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    float b;
+    double c;
+    char d;
+    cout << "the size of integer:" << sizeof(a) << endl;
+    cout << "the size of float:" << sizeof(b) << endl;
+    cout << "the size of double:" << sizeof(c) << endl;
+    cout << "the size of char:" << sizeof(d) << endl;
+    return 0;
+}
